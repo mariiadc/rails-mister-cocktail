793dfe53a05554861f56a672d1139f9330384e49
Mariia Campos <mariia.dcampos@gmail.com>
2020-02-20 22:23:11 +0000

    one rake point is missing, styling is still missing

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,38 @@
+class DosesController < ApplicationController
+  # params[:dose] = {"description"=>"2 L", "ingredient_id"=>"104"}
+
+  def new
+    @dose = Dose.new
+  end
+
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to @cocktail # cocktail_path(@cocktail)
+    else
+      # Always relative do app/views
+      # This is rendering app/views/cocktails/show.html.erb
+      # Because that's where the form is!
+      render 'cocktails/new'
+    end
+  end
+
+  # DELETE /cocktails/:cocktail_id/doses/:id - if it was nested!
+  # DELETE /doses/:id - not nested :)
+  def destroy
+    dose = Dose.find(params[:id])
+    # cocktail = dose.cocktail
+    dose.destroy
+
+    # redirect_to cocktail_path(cocktail)
+    redirect_to cocktail_path(dose.cocktail)
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id, :cocktail_id)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+
+url = open('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list').read
+result = JSON.parse(url)
+
+result['drinks'].map do |drink|
+  Ingredient.create(name: drink['strIngredient1'])
+end
